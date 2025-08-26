1faa571b191f098e358c43a35c5e57116c9ca503
SageMiller25 <sage.miller@berkeley.edu>
2025-08-26 13:45:10 -0700

    updated 03 conditionals and otter grader

---
+++ 00 Intro to Python/tests/03_intro_to_python/q1.py
@@ -0,0 +1,11 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1',
+    'points': None,
+    'suites': [   {   'cases': [   {   'code': ">>> assert sum_not_equals(7, 7) == 'x and y are equal use multiplication by 2'\n>>> assert sum_not_equals(11, 101) == 112\n",
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
